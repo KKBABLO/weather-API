aa60239a8b66c8c6cea8bc46bbe6e45a256ec125
KKBABLO <vadavang@gmail.ru>
2021-10-23 16:51:02 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+const api = {
+    key: "207149e9ed79c1044eca04ed10df5997",
+    baseurl: "https://api.openweathermap.org/data/2.5/",
+}
+
+const searchBox = document.querySelector(".search-box");
+
+searchBox.addEventListener("keypress", setQuery);
+
+function setQuery(e) {
+    if(e.keyCode == 13) {
+			getResults(searchBox.value);
+      console.log(searchBox.value);
+      searchBox.value = ""
+    }
+}
+ 
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=metric&APPID=${api.key}`)
+		.then((weather) => {
+			return weather.json();
+		}).then(displayResults);
+}
+
+function displayResults(weather) {
+	console.log(weather);
+	let city = document.querySelector(".location .city")
+	city.innerHTML = `${weather.name}, ${weather.sys.country}`; 
+
+	let now = new Date();
+	let date = document.querySelector(".date")
+	date.innerHTML = dateBuilder(now);
+
+	let temp = document.querySelector(".temp");
+	temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+	let weatherEl = document.querySelector(".weather")
+	weatherEl.innerHTML = weather.weather[0].main;
+	
+	let hiLow = document.querySelector(".hi-low")
+	hiLow.innerHTML = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`
+}   
+ 
+function dateBuilder(s) {
+	let months = ["January", "February", "March", "April", "May", "June", "August", "September", "October", "November", "December"];
+	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+	let day = days[s.getDay()];
+	let date = s.getDate();
+	let month = months[s.getMonth()];
+	let year = s.getFullYear();
+
+	return `${day} ${date} ${month} ${year}`;
+}
